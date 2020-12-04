f26744fe41d98e77e90af8cba5566f2010ae97e4
webnetkz <8gg001@gmail.com>
2020-12-04 11:56:39 +0600

    update

--- index.php
+++ index.php
@@ -1,13 +1,32 @@
+<?php
+
+session_start();
+
+require_once 'app/lib/Debug/DebugMod.php';
+
+?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="helper.css">
-    <title>Document</title>
-</head>
-<body>
+    <head>
+        <title><?=$title?></title>
+
+        <meta charset="UTF-8">
+        <meta name="theme-color" content="rgb(0, 0, 0)">
+        <meta name="author" content="TOO WebNet">
+        <meta name="description" content="Made in WebNet">
+        <meta name="keywords" content="WebNet">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="robots" content="index, follow">
+
+        <link rel="shortcut icon" href="/public/img/miniLogo.png" type="image/png">
+        <link rel="apple-touch-icon" href="/public/img/logo.png">
+        <link rel="stylesheet" href="/public/css/frotHelper.css">
+        <link rel="manifest" href="/public/json/manifest.json">
+        
+    </head>
+
+    <body>
+        
     <div class="dropdown">
         <button>Button</button>
         <div>
@@ -58,5 +77,18 @@
     <div>
         <input type="text" class="inp">
     </div>
-</body>
+
+
+
+
+        <script>
+             // Проверка браузера на поддержку service worker
+            if('serviceWorker' in navigator) {
+                navigator.serviceWorker
+                    .register('public/sw.js')
+                    .then(function() { console.log("Service Worker Registered"); });
+            }
+        </script>
+            <script src="public/js/frontHelper.js"></script>
+    </body>
 </html>
