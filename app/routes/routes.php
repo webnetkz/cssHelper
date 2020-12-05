14b2367575decb5b3c99d5a60c1a961175d1b7bf
webnetkz <8gg001@gmail.com>
2020-12-05 22:01:31 +0600

    update

--- app/routes/routes.php
+++ app/routes/routes.php
@@ -8,11 +8,11 @@ $routes = [
 
 $url = $_SERVER['REQUEST_URI'];
 foreach($routes as $k => $v){
-    echo $v.'<br>';
     if($url == $v) {
-        var_dump($routes);
+        echo $v.'<br>';
+        //var_dump($routes);
     } else {
-        header('Location: https://google.com');
+        //header('Location: https://google.com');
     }
 
 }
